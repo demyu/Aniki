d1d67175f09edec8b1413777f276352038d1a818
demyu <demyujanus@yahoo.com>
2020-09-01 17:17:32 +0800

    Search Update

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,7 +1,7 @@
 import 'package:aniki_app/Widgets/airing.dart';
+import 'package:aniki_app/Widgets/searchAnime.dart';
 import 'package:flutter/material.dart';
 import 'Widgets/categorySelector.dart';
-import 'package:marquee/marquee.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,68 +9,128 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isSearching = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            actions: <Widget>[
-              isSearching
-                  ? IconButton(
-                      icon: Icon(Icons.cancel),
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          this.isSearching = !this.isSearching;
-                        });
-                      })
-                  : IconButton(
-                      icon: Icon(Icons.search),
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          this.isSearching = !this.isSearching;
-                        });
-                      })
-            ],
-            bottom: TabBar(tabs: [
-              Tab(
-                  child: Marquee(
-                text: 'Watching      ',
-              )),
-              Tab(
-                child: Text('Airing'),
-              ),
-              Tab(
-                child: Marquee(
-                  text: 'Next Season      ',
-                ),
-              ),
-              Tab(
-                child: Text('Trending'),
-              ),
-            ]),
-            title: !isSearching
-                ? Text('Aniki')
-                : TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                        icon: Icon(Icons.search, color: Colors.white),
-                        hintText: 'Whats On Your Mind',
-                        hintStyle: TextStyle(color: Colors.white)),
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text('Aniki'),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.white,
+              onPressed: () {
+                showSearch(context: context, delegate: DataSearch());
+              })
+        ],
+      ),
+      body: Column(children: <Widget>[
+        categorySelector(),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.white),
+            child: Airing(),
           ),
-          body: TabBarView(
-            children: [
-              Text("airingList"),
-              Airing(),
-              Text("nextSeasonList"),
-              Text("trendingList"),
-            ],
-          ),
-        ));
+        )
+      ]),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          var query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+        icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow,
+          progress: transitionAnimation,
+        ),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if(query != ""){
+      var search = query.toString();
+    var quer = """
+        query {
+          Page(page: 1, perPage: 5){
+    pageInfo {
+      total
+      currentPage
+      lastPage
+      hasNextPage
+    }
+    media(search: "$query" ,type: ANIME, sort: POPULARITY_DESC){
+      id
+      title {
+        romaji
+        english
+      }
+      characters{
+        edges{
+          node{
+            name{
+              full
+            }
+            image{
+              medium
+            }
+          }
+          voiceActors(language: JAPANESE) {
+            name {
+              full
+            }
+            image{
+              medium
+            }
+          }
+          
+        }
+      }
+      description
+      coverImage{
+        large
+        medium
+      }
+      status
+      bannerImage
+    }
+  }
+        }
+  """;
+    return SearchAnime(quer: quer);
+  }
+    
+    return Text(query); 
+  
   }
 }
